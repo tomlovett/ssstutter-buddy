a0e2300b7ca9682697c8700b2320098e9f815369
Tom Lovett <tomlovett6@gmail.com>
2024-08-31 11:01:47 -0400

    run rubocop -A

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 module ControllerSpecHelper
   def token_generator(user_id)
-    JsonWebToken.encode(user_id: user_id)
+    JsonWebToken.encode(user_id:)
   end
 
   def expired_token_generator(user_id)
-    JsonWebToken.encode({ user_id: user_id }, (Time.now.to_i - 10))
+    JsonWebToken.encode({ user_id: }, (Time.now.to_i - 10))
   end
 
   def valid_headers
     {
-      "Authorization" => token_generator(user.id),
-      "Content-Type" => "application/json"
+      'Authorization' => token_generator(user.id),
+      'Content-Type' => 'application/json'
     }
   end
 
   def invalid_headers
     {
-      "Authorization" => nil,
-      "Content-Type" => "application/json"
+      'Authorization' => nil,
+      'Content-Type' => 'application/json'
     }
   end
 end
