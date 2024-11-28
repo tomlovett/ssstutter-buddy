f257e0e01dc9eabf20267a94ea6cf664d9ef12bf
Tom Lovett <tomlovett6@gmail.com>
2024-11-27 22:07:07 -0500

    touch up manifest; other housekeeping

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+
+
+// was removed because it was crashing the build
+ // link_tree ../../../vendor/javascript .js
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_10_03_002706) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "participants", force: :cascade do |t|
     t.string "codename"
     t.string "country"
