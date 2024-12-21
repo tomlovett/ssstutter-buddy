09881d0c7ed944cddbc7b74a471b0818c8250bee
Tom Lovett <tomlovett6@gmail.com>
2024-12-21 13:17:55 -0500

    split routes by env/prod -- yes that is gross

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, except: :index, path: 'u'
-  get '/users/:id/select_role', to: 'users#select_role'
-  get '/users/:id/change_password', to: 'users#change_password'
+  if Rails.env.production?
+    root 'placeholder#landing'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    get '/faq', to: 'placeholder#faq'
+    match '/*any', to: redirect('/'), via: :all
+  else
+    resources :users, except: :index, path: 'u'
+    get '/users/:id/select_role', to: 'users#select_role'
+    get '/users/:id/change_password', to: 'users#change_password'
 
-  resources :participants, except: :index, path: 'p'
-  resources :researchers, except: :index, path: 'r'
-  resources :studies, path: 's'
+    # Defines the root path route ("/")
+    # root "articles#index"
+
+    resources :participants, except: :index, path: 'p'
+    resources :researchers, except: :index, path: 'r'
+    resources :studies, path: 's'
+  end
 end
