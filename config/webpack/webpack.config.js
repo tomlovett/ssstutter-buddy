90e678dee50b7453e252ed4402316ba49847fdac
Tom Lovett <tomlovett6@gmail.com>
2025-01-18 20:12:11 -0300

    re-add bootstrap for production route

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -2,12 +2,13 @@
 
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig } = require('shakapacker')
+const path = require('path')
 
 const options = {
   resolve: {
     alias: {
-      '@': path.resolve('./'),
-      components: path.resolve('./javascript/components'),
+      '@': path.resolve(__dirname + './'),
+      '@components': path.resolve(__dirname + '/javascript/components'),
     },
   },
 }
