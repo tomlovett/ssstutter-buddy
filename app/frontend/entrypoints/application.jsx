dba230f21419c574fd4c1042dc72a53f0871cbfa
Tom Lovett <tomlovett6@gmail.com>
2025-02-05 00:31:27 -0300

    update Prettier rules on arrow parenthees

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 const pages = import.meta.glob('../pages/**/*.jsx', { eager: true })
 
 createInertiaApp({
-  resolve: (name) => pages[`../pages/${name}.jsx`],
+  resolve: name => pages[`../pages/${name}.jsx`],
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
