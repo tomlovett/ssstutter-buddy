9bb8f4f6391c117f2d69b440ca6fe6067ed8ff67
Lukundu Lako <lukundu.lako@gmail.com>
2025-02-16 10:23:17 -0800

    enable shadcn for application (#35)

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -1,10 +1,16 @@
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
+import {Layout} from '../components/Layout/Layout'
+import "../styles/base.css"
 
 const pages = import.meta.glob('../pages/**/*.jsx', { eager: true })
 
 createInertiaApp({
-  resolve: name => pages[`../pages/${name}.jsx`],
+  resolve: name => {
+   const  page = pages[`../pages/${name}.jsx`]
+   page.default.layout = page.default.layout || (page => <Layout children={page} />)
+   return page
+  },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby' // unsure
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [RubyPlugin(), react()],
+  plugins: [RubyPlugin(), react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app/frontend'),
