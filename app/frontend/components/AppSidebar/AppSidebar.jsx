c908ed50e6b9dff07b92d4feae09ba88c2bab263
Lukundu Lako <lukundu.lako@gmail.com>
2025-03-03 16:21:12 -0500

    update: styled participants page (#38)

---
+++ app/frontend/components/AppSidebar/AppSidebar.jsx
@@ -0,0 +1,50 @@
+import { UserRound, Inbox } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Profile",
+    url: "#",
+    icon: UserRound,
+  },
+]
+
+ 
+const AppSidebar = ()=> {
+  return (
+<Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>ssstutter buddy
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
+
+export { AppSidebar }
