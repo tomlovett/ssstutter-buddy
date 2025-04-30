82d82cb1cc2982b38733e3aa070f3dc1b88dca96
Tom Lovett <tomlovett6@gmail.com>
2025-04-29 20:43:13 -0400

    fix pathing issue

---
+++ app/frontend/lib/utils.jsx
@@ -0,0 +1,28 @@
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export const cn = (...inputs) => {
+  return twMerge(clsx(inputs))
+}
+
+export const capitalize = str =>
+  str && str[0].toLocaleUpperCase() + str.substring(1)
+
+// Tue Oct 17
+export const formatDate = dateObj =>
+  new Date(dateObj).toDateString().slice(0, -4)
+
+//  Oct 17 , 1995
+export const formatBirthday = dateObj => {
+  const date = new Date(dateObj)
+
+  return date.toDateString().slice(4, -5) + ', ' + date.getUTCFullYear()
+}
+
+export const formatLocation = (country, state, city) => {
+  if (!country) {
+    return 'No Location Selected'
+  }
+
+  return `${city.name || city}, ${state.name || state}, ${country.name || country}`
+}
