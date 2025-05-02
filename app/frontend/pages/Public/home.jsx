da1af12913d83c1e61cec356c78ecab26a89bd57
Tom Lovett <tomlovett6@gmail.com>
2025-05-01 22:32:56 -0400

    remove unnecessarily escaped quotes

--- app/frontend/pages/Public/home.jsx
+++ app/frontend/pages/Public/home.jsx
@@ -16,13 +16,13 @@ const Home = () => {
             <p className="text-gray-600">
               You might be surprised, but the biggest impediment to stuttering
               research is usually not funding, research ideas, even technology —
-              it\'s finding enough PWS to participate in the study!
+              it's finding enough PWS to participate in the study!
             </p>
             <p className="mt-4 text-gray-600">
               If you or your child have a stutter,{' '}
               <b>you can support hard-working researchers</b> as they advance
-              our knowledge of stuttering\'s causes, expression, and treatment
-              by particiating in studies.
+              our knowledge of stuttering's causes, expression, and treatment by
+              particiating in studies.
             </p>
             <p className="mt-4 text-gray-600">
               As a nice bonus, most studies compensate participants for their
@@ -31,8 +31,8 @@ const Home = () => {
             </p>
             <p className="mt-4 text-gray-600">
               SSStutterBuddy is entirely free-to-use for both researchers and
-              PWS. We\'re not here to make money,{' '}
-              <b>we\'re here to make a difference</b>
+              PWS. We're not here to make money,{' '}
+              <b>we're here to make a difference</b>
             </p>
             <p className="mt-4 text-gray-600 text-center text-lg underline">
               SSStutterBuddy launches Summer 2025
