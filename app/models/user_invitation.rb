abc056bbcea8c2f905f244f639e860c5cea4c549
Tom Lovett <tomlovett6@gmail.com>
2025-08-01 15:24:29 -0400

    touchup

--- app/models/user_invitation.rb
+++ app/models/user_invitation.rb
@@ -4,14 +4,14 @@ class UserInvitation < ApplicationRecord
   normalizes :email, with: ->(e) { e.strip.downcase }
   validates :email, presence: true, uniqueness: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP }
-  validate :email_not_belongs_to_existing_user, on: :create
+  validate :email_does_not_belong_to_existing_user, on: :create
   validates :invited_by_id, presence: true
 
   scope :pending, -> { where(accepted_at: nil) }
 
   private
 
-  def email_not_belongs_to_existing_user
+  def email_does_not_belong_to_existing_user
     return if email.blank?
 
     errors.add(:email, 'belongs to an existing user') if User.exists?(email:)
