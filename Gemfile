ba2419778a72eddc26dd22d2c8ad0e4b9c361a7c
Tom Lovett <tomlovett6@gmail.com>
2025-08-07 17:35:14 -0400

    add basic Sentry setup

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'puma', '~> 5.0'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
+gem 'sentry-rails'
+gem 'sentry-ruby'
+
 # importmap
 gem 'importmap-rails'
 
--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -1,7 +1,19 @@
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 import { Layout } from 'components/Layout/Layout'
+import * as Sentry from '@sentry/react'
 import '@/styles/base.css'
+
+// Initialize Sentry
+Sentry.init({
+  dsn: 'https://d7d480213057d96b54e11a22ea0ee90a@o4509804242337793.ingest.us.sentry.io/4509804243189760',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 const pages = import.meta.glob('../pages/**/*.jsx', { eager: true })
 
