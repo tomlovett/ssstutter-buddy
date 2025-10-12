e1eb0a8fbc2498fc9ca4d63ad27be46297bb7fe9
Tom Lovett <tomlovett6@gmail.com>
2025-10-12 13:52:53 -0400

    remove puma plugin

--- config/puma.rb
+++ config/puma.rb
@@ -35,9 +35,6 @@ port ENV.fetch('PORT', 3000)
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV.fetch('SOLID_QUEUE_IN_PUMA', nil)
-
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV.fetch('PIDFILE', nil) if ENV.fetch('PIDFILE', nil)
