a4bce95fa7acc9945b45ae8196f26443bd114ca8
Tom Lovett <tomlovett6@gmail.com>
2025-11-02 13:17:54 -0500

    create email health check job

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -21,4 +21,11 @@ class AdminMailer < ApplicationMailer
       subject: "SB: #{@count} New Researcher#{'s' unless @count == 1} - #{@date}"
     )
   end
+
+  def email_health_check
+    mail(
+      to: ENV.fetch('ADMIN_EMAILS', '').split(', '),
+      subject: 'SB emails working'
+    )
+  end
 end
