edb9384e17cfaed0e2b7846e98fbc1022e2c1510
Tom Lovett <tomlovett6@gmail.com>
2025-11-08 11:52:01 -0500

    remove column cleanup for failed prod migration

--- db/migrate/20251108160436_update_studies_add_irb_number_remove_digital_columns.rb
+++ db/migrate/20251108160436_update_studies_add_irb_number_remove_digital_columns.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 class UpdateStudiesAddIrbNumberRemoveDigitalColumns < ActiveRecord::Migration[8.0]
-  def up
-    change_table :studies, bulk: true do |t|
-      t.string :irb_number
-      t.remove :digital_only
-      t.remove :digital_friendly
-    end
-  end
-
-  def down
-    change_table :studies, bulk: true do |t|
-      t.remove :irb_number
-      t.boolean :digital_only, null: false, default: false
-      t.boolean :digital_friendly, null: false, default: false
-    end
+  def change
+    add_column :studies, :irb_number, :string
   end
 end
