6af083b54c96ca9f368a425d91c6cf3bf15755c1
Tom Lovett <10868840+tomlovett@users.noreply.github.com>
2025-11-15 10:28:48 -0500

    fine-tune email list on publish; write more validations before allowing publishing (#51)

--- app/services/publish_study.rb
+++ app/services/publish_study.rb
@@ -4,38 +4,52 @@ class PublishStudy
   def initialize(study:)
     @study = study
     @errors = []
+    @participants_to_exclude = []
   end
 
   def call
-    return false unless valid?
+    return @errors unless valid?
 
-    if @study.published_at.nil?
-      @study.update!(published_at: Time.current)
-    else
-      @study.update!(paused_at: nil, closed_at: nil)
+    return @errors if @study.status == 'active'
+
+    @study.update!(published_at: Time.current, paused_at: nil, closed_at: nil)
+
+    # existing connections
+    @participants_to_exclude.concat(@study.connections.pluck(:participant_id)) if @study.connections.present?
+    # existing invitations
+    @participants_to_exclude.concat(@study.invitations.pluck(:participant_id)) if @study.invitations.present?
+
+    if @study.min_age.present?
+      @participants_to_exclude.concat(Participant.where('birthdate > ?', @study.min_age.years.ago).pluck(:id))
+    end
+    if @study.max_age.present?
+      @participants_to_exclude.concat(Participant.where(birthdate: ...@study.max_age.years.ago).pluck(:id))
     end
 
-    send_invitation_emails unless @study.digital_only?
-  end
+    if @study.location_type == Study::IN_PERSON
+      @participants_to_exclude.concat(Participant.where.not(id: local_participants))
+    end
 
-  private
-
-  def send_invitation_emails
-    # return if @study.digital_only? || @study.location.nil?
-
-    existing_connections = @study.connections.pluck(:participant_id)
-    existing_invitations = @study.invitations.pluck(:participant_id)
-    participants = Participant.joins(:location)
-      .where.not(location: nil)
-      .where.not(id: existing_connections)
-      .where.not(id: existing_invitations)
-    # .near(@study.location.coordinates, 100)
-
-    participants.each do |participant|
+    Participant.where.not(id: @participants_to_exclude).find_each do |participant|
       Invitation.create!(study: @study, participant:, status: :invited)
 
       ParticipantMailer.with(study: @study, participant:).new_study_alert.deliver_later
     end
+
+    [] # return empty errors array
+  end
+
+  private
+
+  def local_participants
+    # `to_a` syntax prevents a SQL error where the scope is joined with the Participant model
+    nearby_location_ids = Location.where.not(participant_id: nil)
+      .near([@study.location.latitude, @study.location.longitude], 100)
+      .to_a.pluck(:id)
+
+    Participant.joins(:location)
+      .where(location: { id: nearby_location_ids })
+      .pluck(:id)
   end
 
   def valid?
@@ -43,6 +57,7 @@ class PublishStudy
     validate_location
     validate_timeline
     validate_age_range
+
     @errors.empty?
   end
 
@@ -51,10 +66,15 @@ class PublishStudy
     required_fields.each do |field|
       @errors << "#{field.to_s.humanize} is required" if @study.send(field).blank?
     end
+
+    @errors << 'Total sessions must be 1 or greater' if @study.total_sessions.to_i < 1
   end
 
   def validate_location
-    return if @study.location_type == Study::DIGITAL
+    if @study.location_type == Study::DIGITAL
+      @study.update!(location: nil)
+      return
+    end
 
     if @study.location.nil?
       @errors << 'Location is required for hybrid or in-person studies'
@@ -68,10 +88,9 @@ class PublishStudy
 
   def validate_timeline
     # Allow duration to be empty if total_sessions == 1
-    return if @study.total_hours.present? && @study.total_sessions.present? &&
-              (@study.total_sessions == 1 || @study.duration.present?)
+    return if @study.total_sessions == 1 || !@study.duration.empty?
 
-    @errors << 'Study timeline (total hours and sessions) must be specified'
+    @errors << 'Study duration must be set if there are multiple sessions'
   end
 
   def validate_age_range
