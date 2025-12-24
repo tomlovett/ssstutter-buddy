3cc6bbbf79bb88dc00f1250a55f988656ebb3afb
Tom Lovett <tomlovett6@gmail.com>
2025-12-24 15:58:07 -0500

    functionality changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_19_235218) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_24_144330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -264,6 +264,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_19_235218) do
     t.string "location_type", default: "in_person"
     t.datetime "last_verified_active", default: -> { "CURRENT_TIMESTAMP" }
     t.string "irb_number"
+    t.boolean "autosend_verified_only", default: true, null: false
     t.index ["researcher_id"], name: "index_studies_on_researcher_id"
   end
 
